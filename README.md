c5a7f44fce46bf0325a3085daab443b7831d2ce0
Eoin Lernihan <eoinblernihan@gmail.com>
2021-10-27 19:11:14 +0100

    rebased file(git issue), mongodb connected

--- README.md
+++
@@ -1,2 +0,0 @@
-# Final-Year-Project-2021-22
-Final year project for gmit 4 year 2021/22
--- aws-lambda-helloworld/src/main/java/com/namitsaxena/aws/lambda/HelloWorldLambda.java
+++ aws-lambda-helloworld/src/main/java/com/namitsaxena/aws/lambda/HelloWorldLambda.java
@@ -2,7 +2,19 @@ package com.namitsaxena.aws.lambda;
 
 import java.util.Map;
 
+import org.bson.Document;
+
 import com.amazonaws.services.lambda.runtime.Context;
+import com.mongodb.BasicDBObject;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 
 /**
@@ -18,9 +30,52 @@ import com.amazonaws.services.lambda.runtime.Context;
  */
 public class HelloWorldLambda 
 {	
+	public static void main(String[] args){
+
+		HelloWorldLambda hello = new HelloWorldLambda();
+		hello.handleRequest(null, null);
+		
+	}
+	
+	
 	public String myHandler(Map<String,Object> input, Context context) 
 	{
 		System.out.println(input);
         return "Hello World";
     }
+	
+	public Object handleRequest(Object o, Context context) {
+
+        System.out.println("welcome to lambda function yeh.!!!");
+        try {
+        	
+        	
+        	ConnectionString connectionString = new ConnectionString("mongodb+srv://user1:bV7hIKFPvWxZqlB8@serverlessinstance0.dgmiv.mongodb.net/ServerlessInstance0?retryWrites=true&w=majority&authSource=admin");
+
+			MongoClientSettings settings = MongoClientSettings.builder()
+        	        .applyConnectionString(connectionString)
+        	        .serverApi(ServerApi.builder()
+        	            .version(ServerApiVersion.V1)
+        	            .build())
+        	        .build();
+        	MongoClient mongoClient = MongoClients.create(settings);
+        	MongoDatabase database = mongoClient.getDatabase("test");
+            System.out.println("welcome to lambda function yeh.!!!");
+
+        	MongoCollection<Document> col = database.getCollection("Names");
+
+            System.out.println("data count" + col.countDocuments());
+
+            BasicDBObject setValue = new BasicDBObject();
+            BasicDBObject query = new BasicDBObject();
+            query.put("contacts.mobileNo", "7090909090");
+            setValue.put("contacts.$.userId", "userId12121212");
+            BasicDBObject set = new BasicDBObject("$set", setValue);
+            mongoClient.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+	}
 }
