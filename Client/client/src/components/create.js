a6b1494ec5fa5dd65703e79705870a1052864030
Eoin Lernihan <eoinblernihan@gmail.com>
2021-12-27 14:21:38 +0000

    Update create.js

--- Client/client/src/components/create.js
+++ Client/client/src/components/create.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const port = 4000;
 
 function getUser() {
-  axios.get(`https://cjh1f85qo9.execute-api.us-east-2.amazonaws.com/Develop/user`)
+  axios.get(`https://cjh1f85qo9.execute-api.us-east-2.amazonaws.com/Develop/userhttps://cjh1f85qo9.execute-api.us-east-2.amazonaws.com/Develop/user`)
             .then(response => {
               console.log(response.data);
                 alert(response.data);
